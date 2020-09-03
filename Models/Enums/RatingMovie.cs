1611d83d067d984dd12a03b4da4a63bc1f831f98
SilviuMihai <silviu96@gmail.com>
2020-09-03 09:43:36 +0300

    Add project files.

---
+++ Models/Enums/RatingMovie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AjaxApp.Models.Enums
+{
+    public enum RatingMovie
+    {
+        Choose,
+        Bad,
+        Good,
+        Fun,
+        Great,
+    }
+}
---
+++ Models/MoviesDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AjaxApp.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AjaxApp.Model
+{
+    public class MoviesDB
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // to generate an ID
+        public string IdMovies { get; set; }
+
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public RatingMovie? RateMovie { get; set;}
+
+        public decimal? Price { get; set; }
+        
+        public DateTime? ReleaseDate { get; set; }
+
+        public string MainCharacter { get; set; }
+
+        public string DirectedBy { get; set; }
+    }
+}
